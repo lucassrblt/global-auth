dd5b90bdf821d7574efe5feec79402ee1e3c91fc
lucas.rimbault <lucas.rimbault@tentelemed.com>
2024-12-18 11:54:56 +0100

    fix: fix statusError on responsehandler

--- server/src/response/responseHandler.ts
+++ server/src/response/responseHandler.ts
@@ -1,7 +1,14 @@
 import { Response } from "express";
 
-export function sendResponse(res: Response, statusCode: number, data: any) {
+export function sendResponse(
+  res: Response,
+  statusCode: number,
+  message: string,
+  data?: any
+) {
   res.status(statusCode).json({
+    statusCode: statusCode === 200 || 201 || 202 || 203 ? "SUCCESS" : "ERROR",
     data: data,
+    message,
   });
 }
